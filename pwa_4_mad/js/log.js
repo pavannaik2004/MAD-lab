7084c8f62a4abd43ed785ab9092e81b91419199b
pavannaik2004 <pavannaik2004@gmail.com>
2026-05-01 08:28:43 +0530

    pwa login

---
+++ pwa_4_mad/js/log.js
@@ -0,0 +1,24 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./js/sw.js")
+      .then((reg) => console.log("SW: Registered successfully!", reg.scope))
+      .catch((err) => console.log("SW: Registration failed:", err));
+  });
+}
+
+localStorage.setItem("username", "rvce");
+localStorage.setItem("passme1", "rvce");
+
+function verify() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let un = localStorage.getItem("username");
+  let pas = localStorage.getItem("passme1");
+  alert(username + " " + password + " " + un + " " + pas);
+  if (un === username && pas === password) {
+    alert("authentication done");
+  } else {
+    alert("authentication failure");
+  }
+}
